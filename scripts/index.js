89aa00ef5c2042db8f65bdf641ee03b0eae79c88
Ryan Yim <yknyim@gmail.com>
2019-03-05 14:35:50 -0500

    adds function for filtering positive and even numbers

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 // add your solutions here
+
+// Positive Numbers
+
+let nums = [-3,-2,-1,0,1,2,3,4];
+function onlyPositive(numbers) {
+    return numbers >= 0
+}
+console.log(nums.filter(onlyPositive))
+
+
+// Even Numbers
+function onlyEven(numbers) {
+    return numbers % 2 === 0
+}
+
+console.log(nums.filter(onlyEven))
+
+// Square the Numbers
