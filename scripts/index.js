aa84ce997bc10f47576b1f4f3b4523b03b490846
Ryan Yim <yknyim@gmail.com>
2019-03-05 14:56:27 -0500

    adds square function

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,51 @@
 
 // Positive Numbers
 
-let nums = [-3,-2,-1,0,1,2,3,4];
-function onlyPositive(numbers) {
-    return numbers >= 0
-}
-console.log(nums.filter(onlyPositive))
+// let nums = [-3,-2,10,4];
+// function onlyPositive(numbers) {
+//     return numbers >= 0
+// }
+// console.log(nums.filter(onlyPositive))
+
+
+
+
+
 
 
 // Even Numbers
-function onlyEven(numbers) {
-    return numbers % 2 === 0
-}
+// function onlyEven(numbers) {
+//     return numbers % 2 === 0
+// }
 
-console.log(nums.filter(onlyEven))
+// console.log(nums.filter(onlyEven))
+
+
+
+
+
+
 
 // Square the Numbers
+
+// function squareTheNumbers(array) {
+//     return array.map(function (x) {
+//         return Math.pow(x,2);
+//     });
+// }
+
+// console.log(squareTheNumbers([4,6,8]))
+
+
+
+
+
+
+// Cities 1
+const cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+];
+
