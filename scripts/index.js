7c9cd8b943c1aac0eec2c159e09d405a50b520ad
Ryan Yim <yknyim@gmail.com>
2019-03-05 15:24:12 -0500

    adds function to list cities by temp and name

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
 
 // let nums = [-3,-2,10,4];
 // function onlyPositive(numbers) {
-//     return numbers >= 0
+//     return numbers >= 0;
 // }
 // console.log(nums.filter(onlyPositive))
 
@@ -16,9 +16,8 @@
 
 // Even Numbers
 // function onlyEven(numbers) {
-//     return numbers % 2 === 0
+//     return numbers % 2 === 0;
 // }
-
 // console.log(nums.filter(onlyEven))
 
 
@@ -43,10 +42,63 @@
 
 
 // Cities 1
-const cities = [
-    { name: 'Los Angeles', temperature: 60.0},
-    { name: 'Atlanta', temperature: 52.0 },
-    { name: 'Detroit', temperature: 48.0 },
-    { name: 'New York', temperature: 80.0 }
+// const cities = [
+//     { name: 'Los Angeles', temperature: 60.0},
+//     { name: 'Atlanta', temperature: 52.0 },
+//     { name: 'Detroit', temperature: 48.0 },
+//     { name: 'New York', temperature: 80.0 }
+// ];
+
+// function printCoolCities(city) {
+//     const allTheKeys = Object.keys(city);
+//     allTheKeys.forEach(function (x) {
+//         if (city[x].temperature <= 70) {
+//             let newCity = city[x];
+//             console.log(newCity)
+//         }
+//     })
+// }
+
+
+
+
+
+// Cities 2
+
+// function printCityNames(town) {
+//     const allTheKeys = Object.keys(town);
+//     allTheKeys.forEach(function (x) {
+//         let towns = town[x].name;
+//         console.log(towns)
+//     })
+// }
+
+
+
+
+
+
+
+
+
+// Good Job!
+
+const people = [
+    'Dom',
+    'Lyn',
+    'Kirk',
+    'Autumn',
+    'Trista',
+    'Jesslyn',
+    'Kevin',
+    'John',
+    'Eli',
+    'Juan',
+    'Robert',
+    'Keyur',
+    'Jason',
+    'Che',
+    'Ben'
 ];
 
+console.log()
